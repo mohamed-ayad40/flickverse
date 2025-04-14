dcb4db1c26897f45be5e6b220519c7e82daf259f
mohamed-ayad40 <mohamedayad5600@gmail.com>
2025-04-14 23:46:45 +0200

    Fixing

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,26 +6,27 @@ export default {
     extend: {
       colors: {
         primary: {
-          50: "#f5f3ff",
-          100: "#ede9fe",
-          200: "#ddd6fe",
-          300: "#c4b5fd",
-          400: "#a78bfa",
-          500: "#8b5cf6",
-          600: "#7c3aed",
-          700: "#6d28d9",
-          800: "#5b21b6",
-          900: "#4c1d95",
+"       " 50: "#f5f3ff",
+ "       "100: "#ede9fe",
+ "       "200: "#ddd6fe",
+ "       "300: "#c4b5fd",
+ "       "400: "#a78bfa",
+ "       "500: "#8b5cf6",
+ "       "600: "#7c3aed",
+ "       "700: "#6d28d9",
+ "       "800: "#5b21b6",
+ "       "900: "#4c1d95",
         },
       },
       fontFamily: {
-        sans: ["Inter", "system-ui", "sans-serif"],
+ "     "sa"s: ["Inte"",""system-ui", "sans-serif"],
       },
       boxShadow: {
-        custom:
-          "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+ 
+         o":
+          "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(", 0, 0, 0.06)",
       },
     },
   },
-  plugins: [],
+  p;ugins: [],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,5 @@ export default defineConfig({
   base: '/',
   build: {
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        // This matches your current file naming pattern
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js'
-      }
-    }
   }
 })
