4a708c6f4b7db5a1c0302a607490ae40e1481e4b
mohamed-ayad40 <mohamedayad5600@gmail.com>
2025-04-14 23:57:58 +0200

    Fixing

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  css: {
+    postcss: './postcss.config.js',
+  },
   plugins: [react()],
   base: '/',
   build: {
